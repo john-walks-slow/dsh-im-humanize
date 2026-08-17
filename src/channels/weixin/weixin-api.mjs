cf47e4aabb26696e7663b3dd71763807eef076bf
xmanrui <841206367@qq.com>
2026-08-17 15:39:32 +0800

    release: prepare dsh-im 0.3.0

--- src/channels/weixin/weixin-api.mjs
+++ src/channels/weixin/weixin-api.mjs
@@ -92,7 +92,7 @@ function authenticatedHeaders(token) {
 function baseInfo() {
   return {
     channel_version: WEIXIN_PROTOCOL_VERSION,
-    bot_agent: 'DeepSeekHarness/0.2.2',
+    bot_agent: 'DeepSeekHarness/0.3.0',
   };
 }
 
